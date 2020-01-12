509b4bbaceb0c5b2209c97a7cf498619df08c716
Jason Safaiyeh <safaiyeh@protonmail.com>
2020-01-12 13:14:13 -0800

    feat(module): Initial Launch of react-native-cookies
    BREAKING CHANGE: Temporary breaking change to trigger semantic major version bump.

---
+++ android/src/main/java/com/reactnativecommunity/cookies/CookieManagerModule.java
@@ -0,0 +1,91 @@
+package com.reactnativecommunity.cookies;
+
+import com.facebook.react.modules.network.ForwardingCookieHandler;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Promise;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URI;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CookieManagerModule extends ReactContextBaseJavaModule {
+
+    private ForwardingCookieHandler cookieHandler;
+
+    public CookieManagerModule(ReactApplicationContext context) {
+        super(context);
+        this.cookieHandler = new ForwardingCookieHandler(context);
+    }
+
+    public String getName() {
+        return "RNCookieManagerAndroid";
+    }
+
+    @ReactMethod
+    public void set(ReadableMap cookie, boolean useWebKit, final Promise promise) throws Exception {
+        throw new Exception("Cannot call on android, try setFromResponse");
+    }
+
+    @ReactMethod
+    public void setFromResponse(String url, String value, final Promise promise) throws URISyntaxException, IOException {
+        Map headers = new HashMap<String, List<String>>();
+        // Pretend this is a header
+        headers.put("Set-Cookie", Collections.singletonList(value));
+        URI uri = new URI(url);
+        try {
+            this.cookieHandler.put(uri, headers);
+            promise.resolve(true);
+        } catch (IOException e) {
+            promise.resolve(false);
+        }
+    }
+
+    @ReactMethod
+    public void getFromResponse(String url, Promise promise) throws URISyntaxException, IOException {
+        promise.resolve(url);
+    }
+
+    @ReactMethod
+    public void getAll(boolean useWebKit, Promise promise) throws Exception {
+        throw new Exception("Cannot get all cookies on android, try getCookieHeader(url)");
+    }
+
+    @ReactMethod
+    public void get(String url, boolean useWebKit, Promise promise) throws URISyntaxException, IOException {
+        URI uri = new URI(url);
+
+        Map<String, List<String>> cookieMap = this.cookieHandler.get(uri, new HashMap());
+        // If only the variables were public
+        List<String> cookieList = cookieMap.get("Cookie");
+        WritableMap map = Arguments.createMap();
+        if (cookieList != null) {
+            String[] cookies = cookieList.get(0).split(";");
+            for (int i = 0; i < cookies.length; i++) {
+                String[] cookie = cookies[i].split("=", 2);
+                if (cookie.length > 1) {
+                  map.putString(cookie[0].trim(), cookie[1]);
+                }
+            }
+        }
+        promise.resolve(map);
+    }
+
+    @ReactMethod
+    public void clearAll(boolean useWebKit, final Promise promise) {
+        this.cookieHandler.clearCookies(new Callback() {
+            public void invoke(Object... args) {
+                promise.resolve(null);
+            }
+        });
+    }
+}
---
+++ ios/react-native-cookies.podspec
@@ -0,0 +1,18 @@
+require 'json'
+
+package = JSON.parse(File.read(File.join(__dir__, '../package.json')))
+
+Pod::Spec.new do |s|
+  s.name                = package['name']
+  s.version             = package['version']
+  s.summary             = package['description']
+  s.homepage            = package['homepage']
+  s.license             = package['license']
+  s.author              = "safaiyeh"
+  s.source              = { :git => "git@github.com:react-native-community/react-native-cookies.git", :tag => "v#{s.version}" }
+  s.requires_arc        = true
+  s.platform            = :ios, "7.0"
+  s.preserve_paths      = "*.framework"
+  s.source_files        = 'RNCookieManagerIOS/*.{h,m}'
+  s.dependency 'React'
+end
