a81edcb64f07924532a73c19ddc0f29a9fafeb6c
Juang, Yi-Lin <frankyjuang@gmail.com>
2020-01-16 07:32:01 +0800

    feat(TypeScript): TypeScript definitions added.
    * Add types
    
    * Update package.json

---
+++ index.d.ts
@@ -0,0 +1,35 @@
+declare module "@react-native-community/react-native-cookies" {
+  export interface Cookie {
+    name: string;
+    value: string;
+    domain: string;
+    path: string;
+    origin: string;
+    version: string;
+    expiration: string;
+  }
+
+  export interface Cookies {
+    [key: string]: string;
+  }
+
+  interface CookieManagerStatic {
+    setFromResponse(url: string, value: string): Promise<boolean | undefined>;
+    clearAll(useWebKit?: boolean): Promise<void>;
+    get(url: string, useWebKit?: boolean): Promise<Cookies>;
+
+    // iOS only.
+    getAll(
+      useWebKit?: boolean
+    ): Promise<{
+      [key: string]: Cookie;
+    }>;
+    set(cookie: Cookie, useWebKit?: boolean): Promise<void>;
+    getFromResponse(url: string): Promise<Cookies>;
+    clearByName(name: string): Promise<void>;
+  }
+
+  const CookieManager: CookieManagerStatic;
+
+  export default CookieManager;
+}
