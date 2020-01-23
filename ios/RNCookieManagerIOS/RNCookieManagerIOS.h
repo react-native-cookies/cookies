de556c1d6ca8e0a34ecaa847e02a05eca1b7aac5
Jason Safaiyeh <safaiyeh@protonmail.com>
2020-01-23 09:36:42 -0800

    chore(docs): Add attribution to Joe Ferraro (#20)
    * Added Joe Ferraro copyright notice
    
    * Update link
    
    * Add copyright to index.js
    
    * Lint

--- ios/RNCookieManagerIOS/RNCookieManagerIOS.h
+++ ios/RNCookieManagerIOS/RNCookieManagerIOS.h
@@ -1,4 +1,10 @@
-// RNCookieManagerIOS.h
+/**		
+  * Copyright (c) Joseph P. Ferraro		
+  *		
+  * This source code is licensed under the MIT license found in the		
+  * LICENSE file here: https://github.com/joeferraro/react-native-cookies/blob/master/LICENSE.md.		
+  */
+
 #if __has_include("RCTBridgeModule.h")
 #import "RCTBridgeModule.h"
 #else
