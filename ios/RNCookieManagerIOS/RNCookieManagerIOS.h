b31b2e941220922ba2d20842d258c64725343f4f
Alessandro Gerelli <geroale2000@gmail.com>
2022-11-08 13:30:54 +0100

    Support for tvOS

--- ios/RNCookieManagerIOS/RNCookieManagerIOS.h
+++ ios/RNCookieManagerIOS/RNCookieManagerIOS.h
@@ -1,13 +1,15 @@
-/**		
-  * Copyright (c) Joseph P. Ferraro		
-  *		
-  * This source code is licensed under the MIT license found in the		
-  * LICENSE file here: https://github.com/joeferraro/react-native-cookies/blob/master/LICENSE.md.		
+/**
+  * Copyright (c) Joseph P. Ferraro
+  *
+  * This source code is licensed under the MIT license found in the
+  * LICENSE file here: https://github.com/joeferraro/react-native-cookies/blob/master/LICENSE.md.
   */
 
 #import <React/RCTBridgeModule.h>
 
+#if __has_include(<WebKit/WebKit.h>)
 #import <WebKit/WebKit.h>
+#endif
 
 @interface RNCookieManagerIOS : NSObject <RCTBridgeModule>
 
